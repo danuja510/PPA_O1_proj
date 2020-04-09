f75e4ecb75d4ed27f667ae2079a7de31407a2810
danuja <danuja.nimsara@gmail.com>
2020-04-10 04:35:54 +0530

    added fname,lname

--- BigGuns/users/forms.py
+++ BigGuns/users/forms.py
@@ -4,14 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email= forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email','first_name', 'last_name', 'password1', 'password2']
 
 class UserUpdateForm(forms.ModelForm):
     email= forms.EmailField()
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username','first_name', 'last_name', 'email']
